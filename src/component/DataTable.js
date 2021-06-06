490923dce5ad8e1fc2ac06b0732af2affbcaa300
deji adesoga <done2stay@gmail.com>
2021-06-06 09:51:46 +0100

    Initial Commit

---
+++ src/component/DataTable.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react'
+import {DataGrid} from '@material-ui/data-grid'
+
+const columns = [
+  {field: 'id', headerName: 'ID'},
+  {field: 'title', headerName: 'Title', width: 300},
+  {field: 'body', headerName: 'Body', width: 600}
+]
+
+const DataTable = () => {
+
+  const [tableData, setTableData] = useState([])
+
+ useEffect(() => {
+   fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((data) => data.json())
+    .then((data) => setTableData(data))
+ })
+
+  return (
+    <div style={{height: 700, width: '100%'}}>
+      <DataGrid 
+        rows={tableData}
+        columns={columns}
+        pageSize={12}
+        checkboxSelection
+      />
+    </div>
+  )
+}
+
+export default DataTable
