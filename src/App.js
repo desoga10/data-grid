e05d77a08d8aabac622ae8c6ac70cb0eaa1ad704
desoga10 <thecodeangle@gmail.com>
2022-01-05 01:43:36 +0100

    Updated DataGrid and Material UI to the latest version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import DataTable from './component/DataTable';
 
--- src/component/DataTable.js
+++ src/component/DataTable.js
@@ -1,29 +1,31 @@
-import React, {useState, useEffect} from 'react'
-import {DataGrid} from '@material-ui/data-grid'
+import React, { useState, useEffect } from 'react'
+import { DataGrid } from '@mui/x-data-grid'
 
 const columns = [
-  {field: 'id', headerName: 'ID'},
-  {field: 'title', headerName: 'Title', width: 300},
-  {field: 'body', headerName: 'Body', width: 600}
+  { field: 'id', headerName: 'ID' },
+  { field: 'title', headerName: 'Title', width: 300 },
+  { field: 'body', headerName: 'Body', width: 600 }
 ]
 
 const DataTable = () => {
 
   const [tableData, setTableData] = useState([])
 
- useEffect(() => {
-   fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((data) => data.json())
-    .then((data) => setTableData(data))
- })
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => data.json())
+      .then((data) => setTableData(data))
+
+  }, [])
+
+  console.log(tableData)
 
   return (
-    <div style={{height: 700, width: '100%'}}>
-      <DataGrid 
+    <div style={{ height: 700, width: '100%' }}>
+      <DataGrid
         rows={tableData}
         columns={columns}
         pageSize={12}
-        checkboxSelection
       />
     </div>
   )
