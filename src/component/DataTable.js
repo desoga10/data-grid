9bded9528e5df1560849b419fc4831f696b77545
desoga10 <thecodeangle@gmail.com>
2022-03-25 16:44:14 +0100

    Made updates

--- src/component/DataTable.js
+++ src/component/DataTable.js
@@ -1,29 +1,41 @@
-import React, {useState, useEffect} from 'react'
-import {DataGrid} from '@material-ui/data-grid'
+import React, { useState, useEffect } from 'react'
+import { DataGrid } from '@material-ui/data-grid'
 
 const columns = [
-  {field: 'id', headerName: 'ID'},
-  {field: 'title', headerName: 'Title', width: 300},
-  {field: 'body', headerName: 'Body', width: 600}
+  { field: 'id', headerName: 'ID' },
+  { field: 'title', headerName: 'Title', width: 300 },
+  { field: 'body', headerName: 'Body', width: 600 }
 ]
 
 const DataTable = () => {
 
   const [tableData, setTableData] = useState([])
 
- useEffect(() => {
-   fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((data) => data.json())
-    .then((data) => setTableData(data))
- })
+  const [rows, setRows] = useState(tableData);
+  const [deletedRows, setDeletedRows] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => data.json())
+      .then((data) => setTableData(data))
+
+  }, [])
+
+  console.log(tableData);
 
   return (
-    <div style={{height: 700, width: '100%'}}>
-      <DataGrid 
+    <div style={{ height: 700, width: '100%' }}>
+      <DataGrid
         rows={tableData}
         columns={columns}
         pageSize={12}
         checkboxSelection
+        onSelectionModelChange={({ selectionModel }) => {
+          const rowIds = selectionModel.map(rowId => parseInt(String(rowId), 10));
+          const rowsToDelete = tableData.filter(row => rowIds.includes(row.id));
+          setDeletedRows(rowsToDelete);
+          console.log(deletedRows);
+        }}
       />
     </div>
   )
